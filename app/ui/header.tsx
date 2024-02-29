cfdce3f246b5a66e30a7ecded359d1ff33e62ea5
bygerman <by.german@outlook.com>
2024-02-29 18:19:51 -0500

    Add header component with Sfx option

---
+++ app/ui/header.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { Switch } from "@nextui-org/react";
+import { SfxOption } from "./shared/sfx-option";
+
+export function Header() {
+  return (
+    <header className="flex justify-between mx-8 my-4">
+      <h1>Music App</h1>
+
+      <div className="flex gap-x-8">
+        <SfxOption></SfxOption>
+        <Switch
+          defaultSelected
+          color="default"
+          size="sm"
+        >
+          Sound
+        </Switch>
+      </div>
+    </header>
+  );
+}
