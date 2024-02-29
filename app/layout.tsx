b197d66d5dc23a51e233de07d86022c6574ab324
bygerman <by.german@outlook.com>
2024-02-28 20:46:26 -0500

    Add background styling to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <div className="background"></div>
       <body className={inter.className}>{children}</body>
     </html>
   );
