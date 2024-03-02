3fa16735be3db0aa1b225f66a5f3ef2636f07f20
bygerman <by.german@outlook.com>
2024-03-01 19:18:34 -0500

    Add grid template for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@nextui-org/react";
 import { Header } from "./ui/header";
+import { Player } from "./ui/player/player";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <Button>Click me</Button>
-      </main>
-    </>
+    <main className={styles.grid_template}>
+      <Header></Header>
+      <div className="content" style={{ border: "1px solid yellow" }}></div>
+      <Player></Player>
+    </main>
   );
 }
---
+++ app/ui/player/player.tsx
@@ -0,0 +1,9 @@
+import { Grenze } from "next/font/google";
+
+export function Player() {
+  return (
+    <div style={{border: "1px solid blue"}}>
+      <h1>Player</h1>
+    </div>
+  );
+}
