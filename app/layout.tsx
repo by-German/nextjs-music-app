37f5c6821ca86371d6cc4fca232e3762d0da3e40
bygerman <by.german@outlook.com>
2024-03-01 19:18:59 -0500

    Add h-screen class to body element in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} h-screen`}>{children}</body>
     </html>
   );
 }
