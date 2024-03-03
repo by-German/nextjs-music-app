09dc6d3cb7e09d4418cabe8825b12492acb4ccf7
bygerman <by.german@outlook.com>
2024-03-02 20:10:54 -0500

    Add favorite icon and rearrange player controls

--- app/ui/player/player-controllers.tsx
+++ app/ui/player/player-controllers.tsx
@@ -4,7 +4,7 @@ import { Slider } from "@nextui-org/react";
 import { PlayIcon } from "../../icons/play.icon"
 import { SkipBackIcon } from "@/app/icons/skip-back.icon";
 import { SkipNextIcon } from "@/app/icons/skip-next.icon";
-
+import { FavoriteIcon } from "@/app/icons/favorite.icon";
 
 export function PlayerControllers() {
   return (
@@ -14,19 +14,30 @@ export function PlayerControllers() {
         <span>0:00</span>
       </div>
 
-      <div className="flex justify-center items-center gap-x-3 mb-4">
-        <Button isIconOnly aria-label="skip-back">
-          <SkipBackIcon filled />
+      <div className="flex justify-evenly items-center mb-4 mt-2">
+
+        <Button isIconOnly aria-label="favorite" size="sm" >
+          <FavoriteIcon filled />
         </Button>
 
-        <Button isIconOnly color="primary" aria-label="play" size="lg">
-          <PlayIcon filled size={30} />
-        </Button>
+        <div className="w-96 flex justify-center items-center gap-x-4">
+          <Button isIconOnly aria-label="skip-back">
+            <SkipBackIcon filled />
+          </Button>
 
-        <Button isIconOnly aria-label="skip-next">
-          <SkipNextIcon filled />
-        </Button>
+          <Button isIconOnly color="primary" aria-label="play" size="lg">
+            <PlayIcon filled size={30} />
+          </Button>
+
+          <Button isIconOnly aria-label="skip-next">
+            <SkipNextIcon filled />
+          </Button>
+        </div>
+
+        // TODO: Add volume control
+
       </div>
+
     </>
   );
 }
