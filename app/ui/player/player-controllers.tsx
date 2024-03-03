fa95d1d8b58511547d0b1da1c0010961714d4d2e
bygerman <by.german@outlook.com>
2024-03-02 20:46:16 -0500

    Add play/pause functionality to player controllers

--- app/ui/player/player-controllers.tsx
+++ app/ui/player/player-controllers.tsx
@@ -1,17 +1,28 @@
+'use client'
+
 import { Button } from "@nextui-org/react";
 import { Slider } from "@nextui-org/react";
 
 import { PlayIcon } from "../../icons/play.icon"
+import { PauseIcon } from "@/app/icons/pause.icon";
 import { SkipBackIcon } from "@/app/icons/skip-back.icon";
 import { SkipNextIcon } from "@/app/icons/skip-next.icon";
 import { FavoriteIcon } from "@/app/icons/favorite.icon";
 import { VolumeController } from "./volume-controller";
+import { useState } from "react";
+
 
 export function PlayerControllers() {
+  const [playing, setPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setPlaying(!playing);
+  }
+
   return (
     <>
       <div className="flex gap-x-4">
-        <Slider aria-label="playtime slider"/>
+        <Slider aria-label="playtime slider" />
         <span>0:00</span>
       </div>
 
@@ -26,8 +37,10 @@ export function PlayerControllers() {
             <SkipBackIcon filled />
           </Button>
 
-          <Button isIconOnly color="primary" aria-label="play" size="lg">
-            <PlayIcon filled size={30} />
+          <Button isIconOnly color="primary" aria-label="play/pause" size="lg"
+            onClick={handlePlay}
+          >
+            {playing ? <PauseIcon filled size={30} /> : <PlayIcon filled size={30} />}
           </Button>
 
           <Button isIconOnly aria-label="skip-next">
